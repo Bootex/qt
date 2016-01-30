670f9528734a6971fb05958e823c50a4e72fb531
Bootex <furorluna@gmail.com>
2016-01-30 16:12:00 +0900

    make notepad2

---
+++ notepad2/notepad.h
@@ -0,0 +1,14 @@
+#include <QtGui>
+
+class Notepad : public QWidget
+{
+	Q_OBJECT
+
+	public:
+			Notepad();
+	private slots:
+			void quit();
+	private:
+			QTextEdit *textEdit;
+			QPushButton *exitButton;
+};
